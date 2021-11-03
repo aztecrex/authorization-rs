9a562b498f34b29048f1d3894f8d7e36f5207dfc
Greg Wiley <aztec.rex@jammm.com>
2021-11-03 13:43:38 -0700

    non-working checkpoint

--- src/api.rs
+++ src/api.rs
@@ -15,6 +15,28 @@ pub trait Query {
     fn authorize(
         &self,
         principal: &Principal<Self::PId>,
-        query: dyn Iterator<Item = (Self::Resource, Self::Action)>,
+        query: &dyn Iterator<Item = (Self::Resource, Self::Action)>,
     ) -> Result<Vec<((Self::Resource, Self::Action), Effect)>, Self::Err>;
+}
+
+impl<T> Query for T {
+    type PId = ();
+    type Err = ();
+    type Resource = ();
+    type Action = ();
+
+    fn authorize(
+        &self,
+        _principal: &Principal<Self::PId>,
+        _query: &dyn Iterator<Item = (Self::Resource, Self::Action)>,
+    ) -> Result<Vec<((Self::Resource, Self::Action), Effect)>, Self::Err> {
+        // 1. fetch policies for each principal
+        // 1.a fetch attached policies
+        // 1.b fetch indirect policies (roles?)
+        // 1c. fetch indirect policy template parameters and apply to templates (associated roles?)
+        // 2. for each query item, compute for each principle
+        // 3. combine strict for each principle
+        // 4. zip with query items
+        todo!();
+    }
 }
